66e03d525b183ae3ddb36d12ff779fad142d8027
weimingjiang <2546266420@qq.com>
2022-10-19 19:38:32 +0800

    完成了主页功能的开发

--- assets/js/baseapi.js
+++ assets/js/baseapi.js
@@ -2,5 +2,22 @@
 // 会先调用 ajaxPrefilter() 这个函数
 // 在这个函数中可以拿到给Ajax提供的配置对象
 $.ajaxPrefilter(function (options) {
+    // 在发起真正的ajax请求之前  统一拼接请求的根路径
     options.url = 'http://api-breakingnews-web.itheima.net' + options.url
+    // 统一为有权限的接口设置headers请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+    // 全局统一挂载complete回调函数
+    options.complete = function (res) {
+        // 在complete回调函数中可以使用res.responseJSON拿到服务器响应回来的数据
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            // 强制清空token
+            localStorage.removeItem('token')
+            // 强制跳转到登录页面
+            location.href = '/login.html'
+        }
+    }
 })
